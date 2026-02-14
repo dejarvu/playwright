9252366e87c8d3fea463a1a80ce437b9449e2600
dejarvu <olatunjiayodeji57@gmail.com>
2026-02-14 20:59:56 +0000

    added a new file network call

---
+++ tests/webApipart1.spec.js
@@ -0,0 +1,93 @@
+const {test, expect, request} = require('@playwright/test')
+const loginPayload = {userEmail: "ayodeji@mailsac.com", userPassword: "Ayodeji123#"};
+let token;
+test.beforeAll ( async() =>
+{
+    const apiContext = await request.newContext ();
+    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+    {
+         data: loginPayload
+    
+    } )
+    expect(loginResponse.ok()).toBeTruthy();
+    const loginResponseJson = await loginResponse.json();
+    const token = loginResponseJson.token;
+    console.log(token)
+
+});
+
+test('Client App Login', async({page}) => 
+{   
+    page.addInitScript(value => {
+
+        window.localStorage.setItem('token', value);
+    }, token );
+    
+    // await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+    // await page.locator('#userEmail').fill(email);
+    // await page.locator('#userPassword').fill('Ayodeji123#');
+    // await page.locator('#login').click()
+    // await page.waitForLoadState('networkidle');
+    // await page.locator('.card-body').first().waitFor();
+    const email = 'ayodeji@mailsac.com';
+    const productName = 'ZARA COAT 3';
+    const products = page.locator('.card-body');
+    const titles = await page.locator('.card-body b').allTextContents();
+    console.log(titles);
+    const count = await products.count();
+    for (let i =0; i < count; ++i ){
+       if(await products.nth(i).locator('b').textContent() ===productName){
+        //add to cart
+        await products.nth(i).locator('text= Add To Cart').click();
+        break;
+    }
+    }
+    await page.locator("[routerlink*='cart']").click()
+    await page.locator('div li').first().waitFor();
+
+    const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible()
+    expect(bool).toBeTruthy();
+    await page.locator('text= Checkout').click();
+    await page.locator("[placeholder*='Country']").pressSequentially('ind', {delay:150});
+    const dropdown = page.locator('.ta-results');
+    await dropdown.waitFor();
+    const optionsCount = await dropdown.locator('button').count();
+    for (let i = 0; i < optionsCount; ++i)
+    {
+        const text = await dropdown.locator('button').nth(i).textContent();
+        if(text === " India")
+        {
+            await dropdown.locator('button').nth(i).click();
+            break;
+
+        }
+    }
+        expect(page.locator(".details__user [type = 'text']").first()).toHaveText(email);
+        await page.locator('.action__submit ').click();
+        await expect(page.locator('.hero-primary')).toHaveText(' Thankyou for the order. ');
+        const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+        console.log(orderId)
+        await page.locator("button[routerlink*='myorders']").click();
+        await page.locator('tbody').waitFor();
+        const rows = await page.locator('tbody tr');
+        
+        for (let i =0; i< await rows.count(); i++)
+        {
+            const rowOrderId = await rows.nth(i).locator('th').textContent();
+            if (orderId.includes(rowOrderId))
+            {
+                await rows.nth(i).locator('button').first().click();
+                break;
+            }
+
+        }
+        const orderIdDetails = await page.locator(';.col-text').textContent();
+        expect (orderId.includes(orderIdDetails)).toBeTruthy();
+
+
+
+
+
+    await page.pause();
+
+});
